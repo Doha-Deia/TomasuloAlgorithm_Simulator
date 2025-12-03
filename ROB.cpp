75b6fe511efd02d00d2792fe56623523782a7ffd
Doha-Deia <doha_deia@aucegypt.edu>
2025-12-03 19:47:09 +0200

    ROB class

--- ROB.cpp
+++ ROB.cpp
@@ -3,3 +3,66 @@
 //
 
 #include "ROB.h"
+#include <stdexcept>
+
+ROB::ROB() : head(0), tail(0), count(0) {
+    for (int i = 0; i < 8; i++) {
+        entries[i].type = "";
+        entries[i].dest = -1;
+        entries[i].value = 0;
+        entries[i].ready = false;
+    }
+}
+
+bool ROB::isFull() const {
+    return count == 8;
+}
+
+bool ROB::isEmpty() const {
+    return count == 0;
+}
+
+void ROB::addEntry(const string& type, int dest) {
+    if (isFull()) {
+        throw std::runtime_error("ROB is full");
+    }
+    entries[tail].type = type;
+    entries[tail].dest = dest;
+    entries[tail].value = 0;
+    entries[tail].ready = false;
+
+    tail = (tail + 1) % 8;
+    count++;
+}
+
+void ROB::markReady(int robNum, int value) {
+    for (int i = 0, idx = head; i < count; i++, idx = (idx + 1) % 8) {
+        if (idx == robNum) {
+            entries[idx].value = value;
+            entries[idx].ready = true;
+            return;
+        }
+    }
+}
+
+ROBEntry* ROB::peek() {
+    if (isEmpty()) return nullptr;
+    return &entries[head];
+}
+
+void ROB::remove() {
+    if (!isEmpty()) {
+        entries[head].type = "";
+        entries[head].dest = -1;
+        entries[head].value = 0;
+        entries[head].ready = false;
+
+        head = (head + 1) % 8;
+        count--;
+    }
+}
+
+const ROBEntry* ROB::getEntries() const {
+    return entries;
+}
+
--- ROB.h
+++ ROB.h
@@ -4,11 +4,36 @@
 
 #ifndef ROB_H
 #define ROB_H
+#include <string>
+#include <queue>
+using namespace std;
 
-
+struct ROBEntry {
+    string type;
+    int dest;
+    int value;
+    bool ready;
+};
 
 class ROB {
+    public:
+        ROB();
 
+        bool isFull() const;
+        bool isEmpty() const;
+
+        void addEntry(const string& type, int dest);
+        void markReady(int robNum, int value);
+        ROBEntry* peek();
+        void remove();
+
+        const ROBEntry* getEntries() const;
+
+    private:
+        int head;
+        int tail;
+        int count;
+        ROBEntry entries[8];
 };
 
 
